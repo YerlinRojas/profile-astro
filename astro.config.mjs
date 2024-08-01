bed8a7da17a49447aee5a3e239e81369ce4b8307
Yerlin ROJAS <yerlinrojas808@gmail.com>
2024-08-01 08:48:23 -0300

    full project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 
 export default defineConfig({
   // ...
   pages: [
-    // Otras páginas estáticas aquí
     './src/pages/index.astro', // Página principal
     './src/pages/projects/[slug].astro', // Página de detalle de producto dinámica
   ],
-  integrations: [tailwind(), react()],
+  integrations: [
+    tailwind(), 
+    react(),
+  ],
+  experimental: {
+    env: {
+      schema: {
+        SERVICE_FORM: envField.string({ context: "client", access: "public" }),
+        TEMPLATE_FORM: envField.string({ context: "client", access: "public" }),
+        KEY_FORM: envField.string({ context: "client", access: "public" })
+      }
+    }
+  }
 });
--- src/components/react/Contact_form.jsx
+++ src/components/react/Contact_form.jsx
@@ -2,6 +2,7 @@ import emailjs from '@emailjs/browser';
 import Swal from 'sweetalert2';
 import { useRef } from 'react';
 import confetti from 'canvas-confetti';
+import {SERVICE_FORM, TEMPLATE_FORM, KEY_FORM} from 'astro:env/client'
 
 const Contact_Form = () => {
     const formRef = useRef(null);
@@ -10,13 +11,13 @@ const Contact_Form = () => {
 
     const sendEmail = (e) => {
         e.preventDefault();
-        emailjs.sendForm("", "", formRef.current, "")
+        emailjs.sendForm(SERVICE_FORM, TEMPLATE_FORM, formRef.current, KEY_FORM)
             .then((res) => {
                 if (res.status === 200) {
                     Swal.fire({
                         icon: "success",
-                        title: "Gracias",
-                        text: "Pronto me contacto contigo",
+                        title: "Thanks",
+                        text: "We will contanct you soon",
                         showConfirmButton: false,
                         timer: 3000,
                     });
@@ -37,7 +38,7 @@ const Contact_Form = () => {
                 Swal.fire({
                     icon: "error",
                     title: "Error",
-                    text: "Hubo un problema al enviar el mensaje. Por favor, intenta de nuevo más tarde.",
+                    text: "We have an error at this moment. Please try again later.",
                 });
             });
     }
@@ -45,28 +46,28 @@ const Contact_Form = () => {
     return (
         <div className="flex flex-row items-center justify-center w-full min-h-screen">
             <div className="w-[40%] flex items-center justify-center image-container">
-                <img ref={imageRef1} className="w-72 default-show" src="/png/bad-01.png" alt="bad" />
-                <img ref={imageRef2} className="w-72 hover-show hidden" src="/gif/i_yerlin2.gif" alt="happy" />
+                <img ref={imageRef1} className="w-5/6 default-show" src="/png/bad-01.png" alt="bad" />
+                <img ref={imageRef2} className="w-5/6 hover-show hidden" src="/gif/i_yerlin2.gif" alt="happy" />
             </div>
 
             <form ref={formRef} className="w-[60%] px-8 py-4" onSubmit={sendEmail}>
                 <div className="mb-5">
-                    <label htmlFor="nombre" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre y Apellido</label>
-                    <input type="text" id="nombre" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nombre Apellido" required name="nombre" />
+                    <label htmlFor="nombre" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                    <input type="text" id="nombre" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Name" required name="Name" />
                 </div>
                 <div className="mb-5">
                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="user@email.com" required name="email" />
+                    <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="user@email.com" required name="Email" />
                 </div>
                 <div className="mb-5">
-                    <label htmlFor="telefono" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Telefono</label>
-                    <input type="tel" id="telefono" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="450 2011 0115" required name="telefono" />
+                    <label htmlFor="telefono" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone number</label>
+                    <input type="tel" id="telefono" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="450 2011 0115" required name="Phone number" />
                 </div>
                 <div className="mb-5">
-                    <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Dejanos tu consulta</label>
-                    <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Deja tu mensaje" name="consulta"></textarea>
+                    <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Message</label>
+                    <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Message" name="message"></textarea>
                 </div>
-                <button type="submit" className="text-white bg-[#C81010] hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm w-full sm:w-auto px-12 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">ENVIAR</button>
+                <button type="submit" className="text-white bg-[#C81010] hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm w-full sm:w-auto px-12 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">SEND</button>
             </form>
         </div>
     )
