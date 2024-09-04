c9a46ea40e6212392305df13d2ccd80038f34822
Yerlin ROJAS <yerlinrojas808@gmail.com>
2024-09-04 15:28:17 -0300

    add categories graphic / architect

---
+++ src/components/react/Slider.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+function Responsive({images}) {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    initialSlide: 1,
+    autoplaySpeed: 3000,
+    autoplay:true,
+    fade:true,
+    
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+          
+         
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows:false,
+          
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows:false,
+                
+        }
+      }
+    ]
+  };
+
+  return (
+    
+       <div class="slider-container  ">
+      <Slider {...settings}>
+        {images.length > 0 ? (
+          images.map((image, index) => (
+            <div key={index}>
+              <img src={image} alt={`Slide ${index + 1}`} />
+            </div>
+          ))
+        ) : (
+          <div>No images available</div>
+        )}
+      </Slider>
+    </div>
+    
+   
+  );
+}
+
+export default Responsive;
